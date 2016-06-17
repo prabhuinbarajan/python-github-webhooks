b42bdc98c719ff96f1c00b32497832dbf14be554
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2016-06-17 12:40:08 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #docker build -t githook -f Dockerfile-wsgi .
-home_dir=/Users/inbpr01/Dev/git/python-github-webhooks
+home_dir=`pwd`
 docker run -d --name qube_githook \
       -v $home_dir/hooks:/app/hooks \
       -v $home_dir/config.json:/app/config.json \
